0ba128181f447d7241a5588442c494d25d419a35
Sainiaashi <aashisaini27012006@gmail.com>
2026-07-25 13:44:07 +0530

    Initial commit: backend project setup

---
+++ src/main/java/com/aashi/QueueEase/QueueEaseApplication.java
@@ -0,0 +1,13 @@
+package com.aashi.QueueEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueEaseApplication.class, args);
+	}
+
+}
