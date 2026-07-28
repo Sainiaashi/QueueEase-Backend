9f8bdb56ee1be1e1eec518d310e6640b7af6588b
Sainiaashi <aashisaini27012006@gmail.com>
2026-07-28 17:37:27 +0530

    Make OTP email sending asynchronous to prevent registration hang on Render

--- src/main/java/com/aashi/QueueEase/QueueEaseApplication.java
+++ src/main/java/com/aashi/QueueEase/QueueEaseApplication.java
@@ -2,8 +2,10 @@ package com.aashi.QueueEase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class QueueEaseApplication {
 
 	public static void main(String[] args) {
